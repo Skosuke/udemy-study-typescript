b3d4cbd7d385b8156e704d264260d377a1b79d73
Skosuke <38691856+Skosuke@users.noreply.github.com>
2022-08-19 14:57:29 +0900

    number-string

---
+++ src/number-string.ts
@@ -0,0 +1,13 @@
+export {};
+
+let year:number = 1992;
+// year = false;
+console.log(year);
+
+let age:number = 0x1e;
+
+console.log(age)
+
+let name:string = 'Kosuke';
+// name = 10;
+console.log(name);
