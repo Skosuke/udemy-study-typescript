d0a6d17a65b70350ff602a353420b1b1e41f994f
Skosuke <38691856+Skosuke@users.noreply.github.com>
2022-08-20 16:47:27 +0900

    object

---
+++ src/object.ts
@@ -0,0 +1,10 @@
+export{};
+
+let profile1: object = { name: 'Kosuke' };
+profile1 = { brithYear: 1992};
+
+let profile2: {
+    name: string;
+} = { name: 'Kosuke'};
+profile2 = { name: 'Kosuke2'};
+console.log(profile2);
