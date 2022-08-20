0927b1b0d3d698f7ae48cd64a05f7e90c6de9153
Skosuke <38691856+Skosuke@users.noreply.github.com>
2022-08-20 17:47:36 +0900

    interface

---
+++ src/interfaces.ts
@@ -0,0 +1,16 @@
+export{};
+
+type ObjectType = {
+    name: string;
+    age: number;
+}
+
+interface ObjectInterFace {
+    name: string;
+    age: number;
+}
+
+let object: ObjectInterFace = {
+    name: 'Kosuke',
+    age: 30
+}
