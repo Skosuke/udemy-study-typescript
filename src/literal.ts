ff4ddc027744d7cda971f36c36c3b7c4ae771ff5
Skosuke <38691856+Skosuke@users.noreply.github.com>
2022-08-21 17:36:38 +0900

    literal

---
+++ src/literal.ts
@@ -0,0 +1,11 @@
+export{};
+
+type dayOfTheWeek = '日' | '月' | '火' | '水' | '木' | "金" | '土'
+let dayOfTheWeek: dayOfTheWeek = '日';
+dayOfTheWeek = '月'
+// dayOfTheWeek = '31'
+
+type month = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+let month: month = 1;
+month = 12;
+// month = 13;
